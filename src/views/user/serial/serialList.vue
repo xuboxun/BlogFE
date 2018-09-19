e4ee29e20691dad4970c5436fee14aeb8344a24b
xbx0119 <huangyifan0119@163.com>
2018-09-19 20:24:42 +0800

    专栏列表页优化

--- src/views/user/serial/serialList.vue
+++ src/views/user/serial/serialList.vue
@@ -2,8 +2,14 @@
     <div class="v-serial-list">
         <div class="serial-item" v-for="item in [1,2,3,4,5]" :key="item">
             <img :src="SerialBG" alt="" class="item-bg">
-            <h3 class="name">专栏名称</h3>
-            <p>最近更新：aaaaaaaaaaaa</p>
+            <h3 class="name">
+                <router-link to="/serial/detail/id">
+                    专栏名称
+                    <Icon name="link" width="14" height="14" />
+                </router-link>
+            </h3>
+            <p class="intro">介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍介绍</p>
+            <p class="update">最近更新：<router-link to="/serial/blog/test">11. ***********</router-link></p>
         </div>
     </div>
 </template>
@@ -24,17 +30,13 @@ export default {
     .serial-item {
         position: relative;
         width: 100%;
-        height: 150px;
+        min-height: 150px;
         box-shadow: 0px 0px 5px #dcdee2;
         margin-bottom: 18px;
         border-radius: 3px;
-        transition: all 0.5s ease;
+        transition: all 2s ease;
         overflow: hidden;
-        padding: 20px 20px;
-
-        &:hover {
-            box-shadow: 0px 0px 15px #ccc;
-        }
+        padding: 15px 20px;
 
         .item-bg {
             position: absolute;
@@ -43,11 +45,38 @@ export default {
             width: 100%;
             height: 100%;
             z-index: -999;
-            opacity: 0.5;
+            opacity: 0.2;
+            transition: all 2s ease;
         }
+
+        &:hover {
+            box-shadow: 0px 0px 15px #ccc;
+
+            .item-bg {
+                transform: scale(1.2);
+            }
+        }
+
         .name {
-            font-size: 1.2rem;
+            font-size: 1.25rem;
             font-weight: 400;
+            color: #17233d;
+        }
+        .intro {
+            // max-height: 60px;
+            line-height: 20px;
+            overflow: hidden;
+            color: #808695;
+            font-size: 0.9rem;
+            text-indent: 1.5rem;
+            padding-left: 1rem;
+            margin-top: 10px;
+            position: relative;
+        }
+        .update {
+            color: #888;
+            font-size: 1.0rem;
+            margin-top: 10px;
         }
     }
 }
