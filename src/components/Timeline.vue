5ba0097efecc4cd1bf78c76ca548ef2dae8d0630
xbx0119 <huangyifan0119@163.com>
2018-09-21 16:28:37 +0800

    添加时间线组件

---
+++ src/components/Timeline.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="m-timeline">
+        <div class="timeline-item" v-for="(item, index) in data" :key="index">
+            <div class="item-bar">
+                <div class="dot" :class="'dot-' + type"></div>
+                <div class="line"></div>
+            </div>
+            <div class="item-content">
+                <p class="title">{{item.title}}</p>
+                <p class="content">{{item.content}}</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            // 点类型：circle空心圆，disc实心圆
+            type: String,
+            default: 'circle',
+            validator: function(val) {
+                return ['circle', 'disc'].indexOf(val) > -1;
+            }
+        },
+        data: {
+            type: Array,
+            default: () => {
+                return [
+                    {
+                        title: '标题标题标题标题标题标题标题标题标题标题标题标题',
+                        content: 'content'
+                    },
+                    {
+                        title: 'title',
+                        content: 'content'
+                    },
+                    {
+                        title: 'title',
+                        content: 'content'
+                    }
+                ];
+            }
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.m-timeline {
+    padding-top: 20px;
+    .timeline-item {
+        min-height: 50px;
+        display: flex;
+        position: relative;
+
+        .item-bar {
+            width: 25px;
+            flex: 0 0 auto;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            position: relative;
+            top: 4px;
+
+            .dot {
+                width: 12px;
+                height: 12px;
+                border-radius: 50%;                
+            }
+            .dot-circle {
+                border: 1px solid #2d8cf0;
+            }
+            .dot-disc {
+                background: #5cadff;
+            }
+            .line {
+                width: 1px;
+                flex-grow: 1;
+                background: #dcdee2;
+            }
+        }
+        &:last-child .item-bar .line {
+            display: none;
+        }
+        .item-content {
+            display: inline-block;
+            min-width: 200px;
+            padding-left: 10px;
+            padding-bottom: 20px;
+
+            .title {
+                line-height: 1.5rem;
+                font-size: 1rem;
+                margin-bottom: 10px;
+            }
+            .content {
+                font-size: 0.9rem;
+            }
+        }
+
+    }
+}
+</style>
