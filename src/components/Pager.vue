e084c79f65262f8e3db573af3770e68e73c602bb
xbx0119 <huangyifan0119@163.com>
2018-09-23 12:03:53 +0800

    添加分页组件

---
+++ src/components/Pager.vue
@@ -0,0 +1,98 @@
+<template>
+    <div class="m-pager">
+        <li class="pager-item"
+            :class="innerPageNum === 1 ? 'pager-item-disabled' : ''"
+            @click="prev"
+        >
+            上一页
+        </li>
+        <li class="pager-item"
+            :class="innerPageNum === page ? 'pager-item-active' : ''"
+            v-for="page in pageCount" :key="page"
+            @click="jump(page)"
+        >
+            {{page}}
+        </li>
+
+        <li class="pager-item"
+            :class="innerPageNum === pageCount ? 'pager-item-disabled' : ''"
+            @click="next"
+        >
+            下一页
+        </li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Pager',
+    props: {
+        pageNum: {
+            type: Number,
+            default: 1,
+        },
+        pageSize: {
+            type: Number,
+            default: 10,
+        },
+        total: {
+            type: Number,
+            default: 105,
+        },
+        onChange: Function
+    },
+    data() {
+        return {
+            innerPageNum: this.pageNum,
+            innerPageSize: this.pageSize,
+        };
+    },
+    methods: {
+        prev() {
+            if (this.innerPageNum !== 1) {
+                this.innerPageNum = this.innerPageNum - 1;
+            }
+        },
+        next() {
+            if (this.innerPageNum !== this.pageCount) {
+                console.log(this.innerPageNum)
+                this.innerPageNum = this.innerPageNum + 1;
+            }
+        },
+        jump(val) {
+            this.innerPageNum = val;
+        },
+        step() {
+
+        }
+    },
+    computed: {
+        pageCount() {
+            return Math.ceil(this.total / this.pageSize);
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+.m-pager {
+    margin-top: 10px;
+    .pager-item {
+        height: 25px;
+        line-height: 25px;
+        float: left;
+        padding: 0 10px;
+        color: #2d8cf0;
+        cursor: pointer;
+    }
+    .pager-item-active {
+        background: #2d8cf0;
+        color: #fff;
+        border-radius: 3px;
+    }
+    .pager-item-disabled {
+        color: #c5c8ce;
+        cursor: not-allowed;
+    }
+}
+</style>
--- src/views/user/tech/techList.vue
+++ src/views/user/tech/techList.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="v-tech-list">
         <BlogItem v-for="tech in blogs" :blog="tech" :key="tech.name"></BlogItem>
+        <Pager></Pager>
     </div>
 </template>
 
 <script>
 import BlogItem from '@/components/BlogItem';
+import Pager from '@/components/Pager';
 export default {
     components: {
         BlogItem,
+        Pager,
     },
     data() {
         return {
