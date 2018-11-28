4704bc5016c23213f1466dd578cdce13dda546ff
xbx0119 <huangyifan0119@163.com>
2018-11-28 12:57:44 +0800

    专栏详情页优化

--- src/views/user/about.vue
+++ src/views/user/about.vue
@@ -3,7 +3,9 @@
         <div class="about-me">
             <h2 class="section-title">关于我</h2>
             <div class="section-content">
-                233
+                <p v-for="(item, index) in intro" :key="index">
+                    <font>{{ item.key }}：</font>{{ item.value }}
+                </p>
             </div>
         </div>
         <div class="about-system">
@@ -26,7 +28,21 @@ export default {
     },
     data() {
         return {
-            versions: []
+            versions: [],
+            intro: [
+                { key: '姓名', value: '徐博勋' },
+                { key: '属性', value: '程序猿, 文艺青年' },
+                { key: '性别', value: '男, 单身求撩o((>ω<))o' },
+                { key: '生日', value: '1997年1月19日' },
+                { key: '邮箱', value: 'ixuboxun@163.com' },
+                { key: '童年', value: '江南水乡, 安徽铜陵' },
+                { key: '我的大学', value: '武汉, 华中农业大学, 计算机科学与技术' },
+                { key: '在人间', value: '浙江, 杭州, 滨江区' },
+                { key: '喜欢的话', value: '你心中的火焰，灼烧的不仅是你的内心，还会成为照亮世界的光，温暖他人' },
+                { key: '书', value: '沉默的大多数, 看见' },
+                { key: '影', value: '无问西东' },
+                { key: '音', value: '网易云音乐, 没有绝对喜欢的歌, 听着好听都喜欢, 偏好老一点的和eason的歌' },
+            ]
         };
     },
     created() {
@@ -52,9 +68,18 @@ export default {
         font-size: 1.2rem;
         font-weight: 400;
         color: #17233d;
+        margin-bottom: 10px;
     }
     .section-content {
         padding: 10px 0 20px 10px;
+        p {
+            color: #888;
+            margin-bottom: 15px;
+            line-height: 22px;
+            font {
+                color: #666;
+            }
+        }
     }
     .about-me {
 
--- src/views/user/serial/serialDetail.vue
+++ src/views/user/serial/serialDetail.vue
@@ -1,19 +1,25 @@
 <template>
     <div class="v-serial-detail">
-        <div class="serial">
-            <h3 class="name">{{ serial.title }}</h3>
-            <p class="desc">{{ serial.description }}</p>
-        </div>
-        <div class="blogs">
-            <h4>博客列表</h4>
-            <ul class="list">
-                <li class="item" v-for="blog in serial.blogs" :key="blog.name">
-                    <router-link :to="'/' + blog.type + '/blog/' + blog.name">
-                        <font class="time">{{Filter.time(blog.createTime)}}</font>
-                        <font class="name">{{blog.title}}</font>
-                    </router-link>
-                </li>
-            </ul>
+        <h3 class="serial-name">{{ serial.title }}</h3>
+        <div class="content-wrapper">
+            <div class="serial">
+                <img class="serial-img" src="/assets/images/serial.jpg" alt="" />
+                <div class="desc">
+                    <h4 class="sec-title">专栏简介</h4>
+                    <p>{{ serial.description }}</p>
+                </div>
+            </div>
+            <div class="blogs">
+                <h4 class="sec-title">博客列表</h4>
+                <ul class="list">
+                    <li class="item" v-for="blog in serial.blogs" :key="blog.name">
+                        <router-link :to="'/' + blog.type + '/blog/' + blog.name">
+                            <font class="time">{{Filter.time(blog.createTime)}}</font>
+                            <font class="name">{{blog.title}}</font>
+                        </router-link>
+                    </li>
+                </ul>
+            </div>
         </div>
     </div>
 </template>
@@ -45,73 +51,92 @@ export default {
 
 <style lang="scss">
 .v-serial-detail {
-    .serial {
-        .name {
-            height: 100px;
-            line-height: 100px;
-            font-size: 1.5rem;
-            font-weight: 400;
-            color: #17233d;
-        }
-        .desc {
-            text-indent: 2.2rem;
-            line-height: 30px;
-            font-size: 1.1rem;
-            color: #515a6e;
+    .serial-name {
+        line-height: 40px;
+        font-size: 1.6rem;
+        font-weight: 400;
+        color: #17233d;
+        margin-bottom: 50px;
+        margin-top: 20px;
+    }
+    @media screen and (max-width: 480px) {
+        .serial-name {
+            text-align: center;
         }
     }
-    .blogs {
-        margin-top: 50px;
-        h4 {
+    .content-wrapper {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+
+        h4.sec-title {
             line-height: 40px;
             border-bottom: 1px solid #e8eaec;
             margin-bottom: 15px;
             font-weight: 400;
-            font-size: 1.1rem;
+            font-size: 1.2rem;
         }
-        .item {
-            line-height: 40px;
-            font-size: 1.15rem;
-            a {
-                display: inline-block;
-                color: #515a6e;
-
-                .time {
-                    margin-right: 15px;
-                    color: #808695;
-                }
-                .name {
+        .serial {
+            .serial-img {
+                width: 100%;
+                border-radius: 5px;
+                margin-bottom: 20px;
+            }
+            .desc {
+                p {
+                    line-height: 30px;
+                    font-size: 1.1rem;
                     color: #515a6e;
                 }
-                &:hover {
-                    .time,
-                    .name {
-                        color: #2d8cf0;
-                    }
-
-                }
             }
 
-            @media screen and (max-width: 480px) {
-                & {
-                    line-height: 35px
-                }
-                a {
-                    .time {
-                        display: inline-block;
-                        width: 100%;
-                        line-height: 20px;
-                        font-size: 0.95rem;
-                        transform: translateY(10px);
-                    }
-                    .name {
-                        display: inline-block;
-                        width: 100%;
-                    }
-                }
-            }
         }
+        .blogs {
+            margin-top: 50px;
+            .item {
+                line-height: 40px;
+                font-size: 1.15rem;
+                a {
+                    display: inline-block;
+                    color: #515a6e;
 
+                    .time {
+                        margin-right: 15px;
+                        color: #808695;
+                    }
+                    .name {
+                        color: #515a6e;
+                    }
+                    &:hover {
+                        .time,
+                        .name {
+                            color: #2d8cf0;
+                        }
+
+                    }
+                }
+
+                @media screen and (max-width: 480px) {
+                    & {
+                        line-height: 35px
+                    }
+                    a {
+                        .time {
+                            display: inline-block;
+                            width: 100%;
+                            line-height: 20px;
+                            font-size: 0.95rem;
+                            transform: translateY(10px);
+                        }
+                        .name {
+                            display: inline-block;
+                            width: 100%;
+                        }
+                    }
+                }
+            }
+
+        }
     }
 }
 </style>
