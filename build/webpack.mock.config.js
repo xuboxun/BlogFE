084c779839f0de93f3938b97184e44e732a991ec
xbx0119 <huangyifan0119@163.com>
2018-12-04 18:45:20 +0800

    server mock

--- build/webpack.mock.config.js
+++ build/webpack.mock.config.js
@@ -1,7 +1,7 @@
 const webpack = require('webpack');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.config.js');
-const mock = require('../mock/mock');
+// const mock = require('../mock/mock');
 
 
 module.exports = merge(base, {
@@ -9,12 +9,16 @@ module.exports = merge(base, {
     devtool: 'source-map',
     devServer: {
         host: '0.0.0.0',
-        port: 8000,
-        contentBase: '../dist',
+        port: 9000,
         proxy: {
-            '/api': 'http://localhost:3000/api'
+            // '/api': 'http://localhost:3000/api'
+            '/api': 'http://blogapi.test.xuboxun.site'
         },
-        before: mock,
+        hot: true,
+        inline: true,
+        historyApiFallback: true,
+        open: true,
+        // before: mock,
     },
     plugins: [
         new webpack.NamedModulesPlugin(),
