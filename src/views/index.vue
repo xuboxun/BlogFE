3cd110c44290d209bd63326fbcd9bf29a0ea26f2
xuboxun <huangyifan0119@163.com>
2019-08-04 15:57:14 +0800

    首页调整

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="v-index">
-        <div class="background" :class="isSupportWebp ? 'webp' : ''"></div>
+    <div class="v-index" v-on:mousemove.passive="handleMouseMove">
+        <div class="background" :class="isSupportWebp ? 'webp' : ''">
+            <div ref="bgImg" class="backImg" :class="isSupportWebp ? 'webp' : ''"></div>
+        </div>
         <div class="content-wrapper" :class="animate ? 'content-wrapper-animate' : ''">
             <span class="toggle" @click="toggle"></span>
             <div class="content">
-                <h1 class="name">徐博勋的博客</h1>
+                <h1 class="name">思吾轩・徐博勋的博客</h1>
                 <div class="entry">
                     <li><router-link to="/home"><Icon name="angle-right" class="icon" />首页</router-link></li>
                     <li><router-link to="/tech"><Icon name="angle-right" class="icon" />技术</router-link></li>
@@ -12,11 +14,10 @@
                     <li><router-link to="/about"><Icon name="angle-right" class="icon" />关于你想知道的我</router-link></li>
                 </div>
                 <div class="link">
-                    <p class="title">快捷链接</p>
+                    <p class="title">全站链接</p>
                     <ul>
-                        <li><a href="https://github.com/xuboxun">github</a></li>
-                        <li><a href="https://www.zhihu.com/">知乎</a></li>
-                        <li><a href="https://www.squarespace.com/">squarespace</a></li>
+                        <li><a href="http://spoos.xuboxun.site/client" target="_blank">对象存储客户端</a></li>
+                        <li><a href="http://axure.xuboxun.site/" target="_blank">Axure交互站</a></li>
                     </ul>
                 </div>
             </div>
@@ -36,6 +37,17 @@ export default {
     methods: {
         toggle() {
             this.animate = !this.animate;
+        },
+        handleMouseMove(e) {
+            const { clientX, clientY } = e;
+            const { clientWidth, clientHeight } = document.body;
+
+            const centerX = Math.floor(clientWidth / 2);
+            const centerY = Math.floor(clientHeight / 2);
+
+            const moveX = -Math.floor((clientX - centerX) / 15);
+            const moveY = -Math.floor((clientY - centerY) / 20);
+            this.$refs.bgImg.style.transform = `translate(${moveX}px, ${moveY}px)`;
         }
     },
     mounted() {
@@ -55,23 +67,36 @@ export default {
         position: absolute;
         top: 0;
         left: 0;
-        background-image: url('/assets/images/sing.jpg');
         background-repeat: no-repeat;
         background-position: center;
+
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
     }
-    .background.webp {
-        background-image: url('/assets/images/sing.webp');
+    .backImg {
+        flex: 1 0 auto;
+        width: 2100px;
+        height: 1200px;
+        background-image: url('/assets/images/girl.jpg');
+        background-position: center center;
+        background-size: auto 100%;
+        transition: all ease-out 0.2s;
+    }
+    .backImg.webp {
+        background-image: url('/assets/images/girl.webp');
     }
     .content-wrapper {
         display: inline-block;
         width: 580px;
         height: 400px;
         position: absolute;
-        top: 40%;
+        top: 45%;
         right: 50%;
         transform: translate(50%, -50%);
         border-radius: 5px;
-        background: rgba(200, 200, 200, 0.4);
+        background: rgba(200, 200, 200, 0.5);
         transition: all ease 0.8s;
 
         .toggle {
@@ -100,8 +125,13 @@ export default {
                 margin-bottom: 20px;
                 text-align: center;
                 font-size: 1.8rem;
-                font-weight: 300;
+                font-weight: 400;
                 color: #17233d;
+            }
+            @media screen and (max-width: 480px) {
+                .name {
+                    font-size: 1.4em;
+                }
             }
             .entry {
                 height: 200px;
@@ -162,11 +192,14 @@ export default {
     }
 
     @media screen and (max-width: 480px) {
-        .background {
+        .backImg {
+            width: 100%;
+            height: 100%;
             background-size: auto 100%;
             background-image: url('/assets/images/index_mb_bg.jpg');
+            transform: none !important;
         }
-        .background.webp {
+        .backImg.webp {
             background-image: url('/assets/images/index_mb_bg.webp');
         }
         .content-wrapper {
